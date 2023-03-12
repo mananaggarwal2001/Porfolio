851054087a1ad07617fd30b815fd6baff04b028d
AnonymousManan <mananaggarwal2001@gmail.com>
2023-03-12 15:40:21 +0530

    hamburger Class Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../CSS/Navbar.css'
+import ham from '../Images/ham.png'
+import cross from '../Images/cross.png'
 const Navbar = () => {
+
+    const [flag, setFlag] = useState(true)
+    const handleToggleButton = () => {
+        document.getElementById('mainHamClass').classList.toggle('icon');
+    }
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className=" navbarSettings container-fluid">
-                <a className="navbar-brand" href="/">MANAN AGGARWAL</a>
-                <div className="buttonPhoneNumber">
-                    <span className='PhoneClass'><i class="fa-solid fa-phone me-2"></i> (+91)-9319054970</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        <div className=' parentContainerOfNavbar d-flex'>
+            <div className=" NavbarContainer d-flex">
+
+                <p className=' nameClass text-uppercase'>Manan Aggarwal</p>
+                <div className="PhoneHamBurgerClass">
+                    <span><i className='fa-solid fa-phone p-2'></i>(+91)-9319054970</span>
+                    <div id='mainHamClass' class="hamclass" onClick={handleToggleButton}>
+                        <div id='bar1' className="bar"></div>
+                        <div id='bar2' className="bar"></div>
+                        <div id='bar3' className="bar"></div>
+                    </div>
                 </div>
-                <div className="collapse navbar-collapse manual-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 totallistitems">
-                        <div className="listcontainer">
-                            <li className="nav-item listitem">
-                                <a className="nav-link mylinksClass" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item listitem">
-                                <a className="nav-link mylinksClass" href="/">About Me</a>
-                            </li>
-                            <li className="nav-item listitem">
-                                <a className="nav-link mylinksClass" href="/">What I Do</a>
-                            </li>
-                            <li className="nav-item listitem">
-                                <a className="nav-link mylinksClass" href="/">Resume</a>
-                            </li>
-                            <li className="nav-item listitem">
-                                <a className="nav-link mylinksClass" href="/">Contact Me</a>
-                            </li>
-                        </div>
-                        <div className="social-icons">
-                            <li className="nav-item listitem ">
-                                <a className="nav-link icon1" href="https://facebook.com" target='_blank' rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
-                            </li>
-                            <li className="nav-item listitem ">
-                                <a className="nav-link icon2" href="https://www.instagram.com/codingwithmanan/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
-                            </li>
-                            <li className="nav-item listitem ">
-                                <a className="nav-link icon3" href="https://www.linkedin.com/in/manan-aggarwal-766603145" target='_blank' rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
-                            </li>
-                            <li className="nav-item listitem ">
-                                <a className="nav-link icon4" href="https://twitter.com/home" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter" ></i></a>
-                            </li>
-                        </div>
-                    </ul>
-                </div>
+
             </div>
-        </nav>
+
+            <div className="listNavItemsClass">
+                <ul className='ListItemClass opacity-invisibleClass'>
+                    <div className='listTopicItemsClass '>
+                        <li> <a href=""> Home </a></li>
+                        <li> <a href=""> About Me</a></li>
+                        <li> <a href=""> What I Do ? </a></li>
+                        <li> <a href=""> Resume </a></li>
+                        <li> <a href=""> Projects </a></li>
+                        <li> <a href=""> Contact Me </a></li>
+                    </div>
+                    <li className='iconClass'>
+                        <a className="icon facebook" href="https://facebook.com" target='_blank' rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
+                        <a className="icon instagram" href="/"><i className="fa-brands fa-instagram"></i></a>
+                        <a className="icon linkedin" href="https://www.linkedin.com/in/manan-aggarwal-766603145" target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+                        <a className="icon twitter" href="/"><i className="fa-brands fa-twitter" target='_blank' rel="noreferrer"></i></a>
+                        <a className="icon github" href="/"><i className="fa-brands fa-github" target='_blank' rel="noreferrer"></i></a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
